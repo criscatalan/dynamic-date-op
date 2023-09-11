3acf44355017ad90d83aa86815ef45ac7883704f
criscatalan <108303825+criscatalan@users.noreply.github.com>
2023-09-11 19:04:59 +0800

    Update date.js

--- date.js
+++ date.js
@@ -1,15 +1,28 @@
-<div id="pd3" style="font-size: 35px; text-align: center; color: #44f814; line-height : 25px; font-weight: 900;">
-        <del style="font-size: 20px; font-weight: 200;">
-        $99.99 USD</del><br>
-        $39.99 USD<br>
-    <font style="font-size:12px; font-weight: 200;">
-        Offer goes here</font>
-</div>
-<script>
-    var precio = 99.99;
-    var descuento = 39.99;
-    var pd3 = document.querySelector("#pd3");
-    fetch("https://ipwhois.app/json/")
-  .then((response) => response.json())
-  .then((data) =>(pd3.innerHTML='<del style="font-size: 20px; font-weight: 200;">'+ data.currency_symbol + new Intl.NumberFormat("en-US").format((data.currency_rates * precio).toFixed(0)) +' ' + data.currency_code +'</del><br>'+ data.currency_symbol + new Intl.NumberFormat("en-US").format((data.currency_rates * descuento).toFixed(0)) +' ' + data.currency_code +'<br><font style="font-size:12px; font-weight: 200;"><img src="'+ data.country_flag +'" width="17px"> Oferta en  ' + data.city + ',  ' + data.country + '</font>'));
+/ Set the date we're counting down to (e.g., New Year's Day)
+var countDownDate = new Date("Jan 1, 2024 00:00:00").getTime();
+
+// Update the countdown every 1 second
+var x = setInterval(function() {
+  // Get the current date and time
+  var now = new Date().getTime();
+
+  // Calculate the remaining time
+  var distance = countDownDate - now;
+
+  // Calculate days, hours, minutes, and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the countdown timer with the default font style
+  var countdownElement = document.getElementById("countdown");
+  countdownElement.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  // If the countdown is over, display a message
+  if (distance < 0) {
+    clearInterval(x);
+    countdownElement.innerHTML = "EXPIRED";
+  }
+}, 1000);
 </script>
